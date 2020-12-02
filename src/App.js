fd06969c4ddcc6ad87059d274416ef9da60de090
maystar236 <70463399+maystar236@users.noreply.github.com>
2020-12-02 21:48:02 +1100

    Added Github details

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
      <h1>Weather App</h1>
+     <footer> 
+       This project was coded by <a href="https://github.com/maystar236" target="_blank">Kirsty</a> and is {" "}
+     <a href="https://github.com/maystar236/React-Weather-App" target="_blank">open-sourced on GitHub</a>
+     </footer>
     </div>
   );
 }
 
-export default App;
+
